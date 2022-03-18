0b31656f35bc17009a514611ba0605cfc66a5177
Jimmy ChangWenChi <geograp232@gmail.com>
2022-03-18 22:00:17 +0800

    修改Port

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ const requestListener = (req,res) => {
 
 const server = http.createServer(requestListener);
 
-server.listen(process.env.Port || 3000);
+server.listen(process.env.PORT || 3000);
 
 //another way to write
 
